d5a46edf8d5bdd27a7fac2ba8a0eb0b567d697b8
Peter Chen <peterchen2017125@gmail.com>
2023-03-23 13:40:54 +0800

    20230323

--- login.php
+++ login.php
@@ -27,7 +27,7 @@
             <label for="">帳號:</label>
             <input type="text" class="form-control my-3" name="user" require>
             <?php if(isset($_SESSION["error"]) && $_SESSION["error"] === "account") {?>
-            <div class="text-danger font-weight-bolder">帳好錯誤</div>
+            <div class="text-danger font-weight-bolder">帳號錯誤</div>
             <?php } ;?>
           </div>
           <div class="my-1">
